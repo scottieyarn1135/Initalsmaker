2f228e3a1d26707dc175d5b7e7effa59711b0ac2
scottyarn <62649689+scottyarn@users.noreply.github.com>
2020-06-23 13:27:44 +0100

    Add files via upload

---
+++ slicing test.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+application_name = "inital maker 3000"
+ai_name = "Billy"
+print("Hello and Thank you for using Yeet initals 3000 I am your trusty RO----AI friend..." )
+sleep(2)
+print(ai_name.upper())
+first_name = input(str("Enter First Name Here: "))
+last_name = input(str("Enter Last Name Here: "))
+
+first_letter = first_name[0]
+last_letter = last_name[0]
+
+print("Hello " + first_name +" "+ last_name)
+sleep(1.5)
+print("Let me make your initials!")
+sleep(3)
+print("Here are your initials!")
+sleep(1.5)
+print(first_letter + last_letter)
+sleep(0.5)
+print("Thank you for using " + application_name.upper() + " Hope you enjoyed using it!")
