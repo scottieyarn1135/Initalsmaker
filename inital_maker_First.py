89e0b746345f0af900c2c2346acc878b72f7494f
scottyarn <62649689+scottyarn@users.noreply.github.com>
2020-06-24 10:15:18 +0100

    Update inital_maker_First.py

--- inital_maker_First.py
+++ inital_maker_First.py
@@ -5,27 +5,27 @@ from time import sleep
 application_name = "inital maker 3000"
 #when the AI name is appears in the print it will say the name of the AI.
 ai_name = "Billy"
-# Start of the app This is mostly to simulate an AI walking you though a process to get the initals of your name.
 
+# Start of the app This is mostly to simulate an AI walking you though a process to get the initals of your name.
 print("Hello and Thank you for using "+ application_name.upper() + " I am your trusty RO----AI friend..." )
 sleep(2)
 print(ai_name.upper())
 sleep(2)
 print("Okay let's get started I will be need both your first name and last name...")
 sleep(2)
+
 #This let's the user input a first name and last name.
 first_name = input(str("Enter First Name Here: "))
 last_name = input(str("Enter Last Name Here: "))
+
 #This grabs the first character of the string for example if the first one is A it will grab A if it is B it will grab B so on and so on.
 first_letter = first_name[0]
 last_letter = last_name[0]
 # it will say Hello to the person using both there first name and last name.
 print("Hello " + first_name +" "+ last_name)
 sleep(1.5)
-# This does not really do anything this is more for show
 print("Let me make your initials!")
 sleep(3)
-# This does not really do anything this is more for show
 print("Here are your initials!")
 sleep(1.5)
 #This prints the first letter of the first name and of the last name.
