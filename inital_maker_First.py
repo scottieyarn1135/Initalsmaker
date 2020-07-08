e27c3f07efb10d9246ef10c085cfc66f47b4ddab
SA <62649689+scottyarn@users.noreply.github.com>
2020-07-08 11:29:31 +0100

    Update inital_maker_First.py

--- inital_maker_First.py
+++ inital_maker_First.py
@@ -1,11 +1,11 @@
-####################### Imported Modules ################################################
+#Imported Modules
 from time import sleep
 
-####################### Variables ########################################################
+# Variables
 application_name = "inital maker 3000"
 ai_name = "Billy"
 
-###################### Start of the app #################################################
+# Start of the app
 print("Hello and Thank you for using "+ application_name.upper() + " I am your trusty RO----AI friend..." )
 sleep(2)
 print(ai_name.upper())
@@ -17,7 +17,7 @@ sleep(2)
 first_name = input(str("Enter First Name Here: "))
 last_name = input(str("Enter Last Name Here: "))
 
-#This grabs the first character of the string for example if the first one is A it will grab A if it is B it will grab B so on and so on.
+#This grabs the first character of the string.
 first_letter = first_name[0]
 last_letter = last_name[0]
 print("Hello " + first_name +" "+ last_name)
@@ -30,8 +30,10 @@ print(first_letter + last_letter)
 sleep(3)
 print("Now creating file!")
 sleep(2)
-#This will create a file called inital.txt this will have text written to the file thanking the user for using the app and it will give them there initals.
+#This will create a file called inital.txt this will have text written to the file.
 with open("inital.txt", mode="w") as f:
-    f.write("Thank you for using "+ application_name + " This file contains your inital don't lose it! \n" + first_letter + last_letter +"\nWe hope you enjoyed using this product!")
+    f.write(f'''Thank you for using {application_name} {first_name} {last_name} 
+                This file contains your inital don't lose it!\n{first_letter}{last_letter}
+                We hope you enjoyed using this product!''')
 print("Thank you for using " + application_name.upper() + " Hope you enjoyed using it!")
 sleep(5)
