2aed63c70cf6ccdfca461663f88bc753370a5548
SA <62649689+scottyarn@users.noreply.github.com>
2020-07-11 14:41:09 +0100

    Add files via upload
    When making the file I wanted it to fit in with PEP so after abit of messing around with how I did the strings I think this fits PEP now.

--- inital_maker_First.py
+++ inital_maker_First.py
@@ -1,6 +1,6 @@
 # Imported Modules
 import time
-# variables of app names
+# Variables of app names
 application_name = "inital maker 3000"
 ai_name = "Billy"
 # Start of the app
@@ -13,7 +13,7 @@ def Introduction():
     time.sleep(2)
 
 Introduction()
-# variables for name and initals.
+# Variables for name and initals.
 first_name = input(str("Enter First Name Here: "))
 last_name = input(str("Enter Last Name Here: "))
 first_letter = first_name[0]
@@ -35,9 +35,9 @@ inital()
 # This will create a file called inital.txt that prints the inital results.
 def filemaker():
     with open('inital.txt', mode='w') as f:
-        f.write(f'''Thank you for using {application_name} {first_name} {last_name}! 
-This file contains your inital don't lose it!\n{first_letter}{last_letter}
-We hope you enjoyed using this product!''')
+        f.write(f"Thank you for using {application_name} {first_name} {last_name}!" 
+                f"This file contains your inital don't lose it!\n{first_letter}{last_letter}\n"
+                "We hope you enjoyed using this product!")
 filemaker()
 print(f"Thank you for using {application_name.upper()} Hope you enjoyed using it!")
 time.sleep(5)
