c479787da54e2add57f17a525661fe46512352f8
SA <62649689+scottyarn@users.noreply.github.com>
2020-07-11 14:18:03 +0100

    Add files via upload
    Changed the code so that they are functions instead.

--- inital_maker_First.py
+++ inital_maker_First.py
@@ -1,39 +1,43 @@
-#Imported Modules
-from time import sleep
-
-# Variables
+# Imported Modules
+import time
+# variables of app names
 application_name = "inital maker 3000"
 ai_name = "Billy"
-
 # Start of the app
-print("Hello and Thank you for using "+ application_name.upper() + " I am your trusty RO----AI friend..." )
-sleep(2)
-print(ai_name.upper())
-sleep(2)
-print("Okay let's get started I will be need both your first name and last name...")
-sleep(2)
+def Introduction():
+    print(f"Hello and Thank you for using {application_name.upper()} I am your trusty RO----AI friend...")
+    time.sleep(2)
+    print(ai_name.upper())
+    time.sleep(2)
+    print("Okay let's get started I will be need both your first name and last name...")
+    time.sleep(2)
 
-#This let's the user input a first name and last name.
+Introduction()
+# variables for name and initals.
 first_name = input(str("Enter First Name Here: "))
 last_name = input(str("Enter Last Name Here: "))
-
-#This grabs the first character of the string.
 first_letter = first_name[0]
 last_letter = last_name[0]
-print("Hello " + first_name +" "+ last_name)
-sleep(1.5)
-print("Let me make your initials!")
-sleep(3)
-print("Here are your initials!")
-sleep(1.5)
-print(first_letter + last_letter)
-sleep(3)
-print("Now creating file!")
-sleep(2)
-#This will create a file called inital.txt this will have text written to the file.
-with open("inital.txt", mode="w") as f:
-    f.write(f'''Thank you for using {application_name} {first_name} {last_name} 
-                This file contains your inital don't lose it!\n{first_letter}{last_letter}
-                We hope you enjoyed using this product!''')
-print("Thank you for using " + application_name.upper() + " Hope you enjoyed using it!")
-sleep(5)
+
+def inital():
+    print(f"Hello {first_name} {last_name}")
+    time.sleep(1.5)
+    print("Let me make your initials!")
+    time.sleep(3)
+    print("Here are your initials!")
+    time.sleep(1.5)
+    print(f"{first_letter}{last_letter}")
+    time.sleep(3)
+    print("Now creating file!")
+    time.sleep(2)
+
+inital()
+# This will create a file called inital.txt that prints the inital results.
+def filemaker():
+    with open('inital.txt', mode='w') as f:
+        f.write(f'''Thank you for using {application_name} {first_name} {last_name}! 
+This file contains your inital don't lose it!\n{first_letter}{last_letter}
+We hope you enjoyed using this product!''')
+filemaker()
+print(f"Thank you for using {application_name.upper()} Hope you enjoyed using it!")
+time.sleep(5)
